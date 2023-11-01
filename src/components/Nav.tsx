c29308726b838e4b883b7ecaf47bb66e683cfc94
Israel Black <israelblack98@outlook.com>
2023-11-02 11:25:33 +1300

    Added box shadow

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 function Nav() {
 	return (
-		<nav className=" fixed w-full flex justify-between px-13 p-5 items-center bg-white border-b-2 border-solid border-black text-blue text-sm">
-			<a href="#" className=" text-2xl  md: hover:underline underline-offset-2">
+		<nav className=" fixed w-full flex justify-between px-13 p-5 items-center bg-white shadow-md text-blue md:font-bold   ">
+			<a
+				href="#"
+				className=" text-2xl hover:underline underline-offset-2 md:text-4xl"
+			>
 				{/* TODO click to top of page, style */}
 				Israel Black
 			</a>
-			<ul className=" space-x-5 text-lg">
-				<a>About</a>
-				<a>Skill</a>
-				<a>Projects</a>
+			<ul className=" space-x-5 text-lg md:text-2xl">
+				<a href="#">About</a>
+				<a href="#">Skill</a>
+				<a href="#">Projects</a>
 			</ul>
 		</nav>
 	);
