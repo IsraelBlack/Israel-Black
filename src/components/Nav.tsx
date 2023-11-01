f45776197e5035dce235078d32ac30bdca3316f6
Israel Black <israelblack98@outlook.com>
2023-11-02 11:05:03 +1300

    Content spacing

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 function Nav() {
 	return (
-		<nav className=" flex justify-between px-9 p-5 items-center bg-white border-b-2 border-solid border-black text-blue text-sm">
-			<a href="#" className=" text-xl  md: hover:underline underline-offset-2">
+		<nav className=" flex justify-between px-13 p-5 items-center bg-white border-b-2 border-solid border-black text-blue text-sm">
+			<a href="#" className=" text-2xl  md: hover:underline underline-offset-2">
 				{/* TODO click to top of page, style */}
 				Israel Black
 			</a>
-			<a className=" ">About</a>
-			<a>Skill</a>
-			<a>Projects</a>
+			<ul className=" space-x-5">
+				<a>About</a>
+				<a>Skill</a>
+				<a>Projects</a>
+			</ul>
 		</nav>
 	);
 }
