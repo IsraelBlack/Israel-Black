4979c3f0ed2cd6825835e82a1d276fdee08b4b84
Israel Black <israelblack98@outlook.com>
2023-11-03 11:14:22 +1300

    populated skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,7 +3,21 @@ import skills from '../data/skills';
 import SkillsItem from './SkillsItem';
 
 function Skills() {
-	return <div></div>;
+	return (
+		<div className="flex flex-col md:flex-row items-center justify-center">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+				{skills.map((project) => (
+					<SkillsItem
+						imgUrl={project.imgUrl}
+						title={project.title}
+						description={project.description}
+						stack={project.stack}
+						link={project.link}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Skills;
