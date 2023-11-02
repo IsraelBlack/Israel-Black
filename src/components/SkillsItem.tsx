927cac73fd7267210b22c1110b6cdac9efd5b33c
Israel Black <israelblack98@outlook.com>
2023-11-03 11:13:55 +1300

    added props

--- src/components/SkillsItem.tsx
+++ src/components/SkillsItem.tsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React from 'react';
 
-function SkillsItem(title:string, imgurl:string, description:string, stack:Array<string>, link:string) {
-  return(
-    <div>
-      <img 
-      src={imgurl}
-      alt="portfolio"
-      className="w-full h-36 md:h-48 object-cover cursor-pointer"
-      />
-      <div className="w-full p-4">
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <p>
-          {stack.map(item => (
-            <span>{item}</span>
-          ))}
-        </p>
-      </div>
-    </div>
-  )
+interface Props {
+	title: string;
+	imgUrl: string;
+	description: string;
+	stack: string[];
+	link: string;
+}
+
+function SkillsItem({ title, imgUrl, description, stack, link }: Props) {
+	return (
+		<div>
+			<img
+				src={imgUrl}
+				alt="portfolio"
+				className="w-full h-36 md:h-48 object-cover cursor-pointer"
+			/>
+			<div className="w-full p-4">
+				<h3>{title}</h3>
+				<p>{description}</p>
+				<p>
+					{stack.map((item) => (
+						<span>{item}</span>
+					))}
+				</p>
+				<a href={link}>Check it Out</a>
+			</div>
+		</div>
+	);
 }
 
 export default SkillsItem;
