62965298e059d84960cc4aa871605bb3d146b220
Israel Black <israelblack98@outlook.com>
2023-11-03 11:32:31 +1300

    cleaned data

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -2,13 +2,13 @@ export default [
 	{
 		title: 'Portfolio Website',
 		imgUrl: '',
-		description: '#',
+		description: '',
 		stack: ['React', 'Vite', 'Tailwind'],
 		link: '#',
 	},
 	{
 		title: 'Book Tracker',
-		imgUrl: '#',
+		imgUrl: '',
 		description:
 			' This projects uses React for a front end with plans to use Mantine Framework for styling. I use knex to create my database with Redux for state management. This project is a chance for me to grow my skills and consolidate my current skills. Next time I build a project like this, I will plan it out more thoroughly to save time while developing ',
 		stack: ['React', 'Knex', 'Redux'],
