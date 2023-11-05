a1cc0f2b9d0f228c6661dfba2de95ddc59d14522
Israel Black <israelblack98@outlook.com>
2023-11-06 10:41:36 +1300

    added MA title

--- src/components/MoreAbout.tsx
+++ src/components/MoreAbout.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function MoreAbout() {
   return(
-    <div></div>
+    <div className="invisible md:visible">
+      <h2 className="font-serif font-bold text-lg md:text-2xl">More About Me</h2>
+    </div>
   )
 }
 
