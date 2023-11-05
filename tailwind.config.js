0638177e704c3968acd15d080a15e5ce1be0cefa
Israel Black <israelblack98@outlook.com>
2023-11-06 10:07:06 +1300

    added cutst boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ export default {
 			blue: '#4756df',
       black: '#0c0c0c',
       orange: '#ff7235',
-      white: '#ffffff'
+      white: '#ffffff',
+      gray: '#6b7280'
 		},
 		extend: {
 			fontFamily: { inter: ['inter', 'serif'] },
+      boxShadow: { 
+        'cust': '0px 2px 15px 2px rgb(107, 114, 128)',
+       }
 		},
 	},
 	plugins: [],
