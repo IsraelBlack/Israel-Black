35ae30be89c3d4da57bb141b3b00fb754a86e251
Israel Black <israelblack98@outlook.com>
2023-11-06 10:07:15 +1300

    Did about

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,8 @@ import ProjectsItem from './ProjectsItem';
 
 function Projects() {
 	return (
-		<div className="flex flex-col md:flex-row items-center justify-center px-5">
+		<div className="flex flex-col md:flex-row items-center justify-center px-5 m-10">
+      <h1>Projects</h1> {/* Resize section Head */}
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 				{projects.map((project) => (
 					<ProjectsItem
