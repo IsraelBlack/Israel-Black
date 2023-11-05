d0bc1aa76a823d97f0aedc39d8ef9dff23c7d066
Israel Black <israelblack98@outlook.com>
2023-11-06 11:22:43 +1300

    added skills data

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,5 +1,20 @@
 export default [
-  {
-    
-  }
-]
+	{
+		imgUrl: '../skills/react.svg',
+	},
+	{
+		imgUrl: '../skills/Typescript.svg',
+	},
+	{
+		imgUrl: '../skills/tailwindcss.svg',
+	},
+	{
+		imgUrl: '../skills/sass.svg',
+	},
+	{
+		imgUrl: '../skills/redux.svg',
+	},
+	{
+		imgUrl: '../skills/git.svg',
+	},
+];
