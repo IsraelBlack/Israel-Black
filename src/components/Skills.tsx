a1a53420b6c7b20eefbf9fa8e6437a82ba6f15e0
Israel Black <israelblack98@outlook.com>
2023-11-06 11:40:03 +1300

    added skills css

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import skills from '../data/skills';
+import SkillsItem from './SkillItem';
 
 function Skills() {
-	return <div>
-    <h1>Top Skills</h1>
-    <div>
-
-    </div>
-  </div>;
+	return (
+		<div className="flex flex-col items-center justify-center px-5 m-10">
+			<h1 className="text-lg md:text-2xl font-bold p-5">Top Skills</h1>
+			<div className="flex md:flex-row ">
+				<div className='grid gridcols-2 md:grid-cols-3 gap-4'>
+					{skills.map((skills) => (
+						<SkillsItem imgUrl={skills.imgUrl} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Skills;
