e89d41e571dfb52aa83b202c53f0ed3edda12ee9
Israel Black <israelblack98@outlook.com>
2023-11-07 10:15:17 +1300

    form methods

--- src/components/EmailMe.tsx
+++ src/components/EmailMe.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 function EmailMe() {
-	return <div className="flex flex-col mb-10 mx-auto">
-    <div className='flex justify-center'>
-      <form></form>
-    </div>
-  </div>;
+	return (
+		<div className="flex flex-col mb-10 mx-auto">
+			<div className="flex justify-center">
+				<form
+					action="#"
+					method="Post"
+					className="flex flex-col w-full md:w-7/12"
+				>
+          
+        </form>
+			</div>
+		</div>
+	);
 }
 
 export default EmailMe;
