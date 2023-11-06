ed33e231fc88611585ddf81d7926354ccf01d775
Israel Black <israelblack98@outlook.com>
2023-11-07 10:13:02 +1300

    added xml

--- src/components/EmailMe.tsx
+++ src/components/EmailMe.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function EmailMe() {
-	return <div></div>;
+	return <div className="flex flex-col mb-10 mx-auto">
+    <div className='flex justify-center'>
+      <form></form>
+    </div>
+  </div>;
 }
 
 export default EmailMe;
