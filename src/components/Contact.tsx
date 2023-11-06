4b4b585c5deced89be192caf7f57e5432df9b658
Israel Black <israelblack98@outlook.com>
2023-11-07 09:55:19 +1300

    icons placed

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 
 function Contact() {
 	return (
-		<div className="flex flex-col fixed right-0 left-1/2">
+		<div className="flex flex-col fixed right-0 bottom-1/2 w-12">
 			<ul>
 				<li>
-					<a
-						href="https://www.linkedin.com/in/israel-black/"
-						target="blank"
-					></a>
-					<img />
+					<a href="https://www.linkedin.com/in/israel-black/" target="blank">
+						<img src="./assets/contacts/icons8-linkedin-circled.gif" />
+					</a>
 				</li>
 				<li>
-					<a href="https://github.com/IsraelBlack" target="blank"></a>
-					<img />
+					<a href="https://github.com/IsraelBlack" target="blank">
+						<img src="./assets/contacts/icons8-github.gif" />
+					</a>
 				</li>
 				<li>
-					<a
-						href="./assets/contacts/Israel-Black-Generic.pdf"
-						target="blank"
-					></a>
-					<img />
+					<a href="./assets/contacts/Israel-Black-Generic.pdf" target="blank">
+						<img src="./assets/contacts/CV.gif" />
+					</a>
 				</li>
 			</ul>
 		</div>
