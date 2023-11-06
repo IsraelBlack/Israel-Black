080e2a0c13455a3ec483417474d1f8fdd5cbabad
Israel Black <israelblack98@outlook.com>
2023-11-07 09:46:35 +1300

    xml

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React from 'react';
 
 function Contact() {
-  return(
-    <div></div>
-  )
+	return (
+		<div className="flex flex-col fixed right-0 left-1/2">
+			<ul>
+				<li>
+					<a
+						href="https://www.linkedin.com/in/israel-black/"
+						target="blank"
+					></a>
+					<img />
+				</li>
+				<li>
+					<a href="https://github.com/IsraelBlack" target="blank"></a>
+					<img />
+				</li>
+				<li>
+					<a
+						href="./assets/contacts/Israel-Black-Generic.pdf"
+						target="blank"
+					></a>
+					<img />
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default Contact;
