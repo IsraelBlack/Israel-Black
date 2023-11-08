805b4eece08a3565ebb960d266672ad563083299
Israel Black <israelblack98@outlook.com>
2023-11-09 09:22:42 +1300

    banner to svg

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import AboutItem from './AboutItem';
 function About() {
 	return (
 		<div className="flex flex-col md:flex-row items-center justify-center bg-gray">
-			<img src="./assets/programmer.jpg" className="w-3/5 h-auto md:w-1/3" />
+			<img src="./assets/programmer.svg" className="w-3/5 h-auto md:w-1/3" />
 			<AboutItem />
 		</div>
 	);
