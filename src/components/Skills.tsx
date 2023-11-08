62f3181abdc98c23fb91eef17bf9f43201eab70d
Israel Black <israelblack98@outlook.com>
2023-11-09 09:36:59 +1300

    title color

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,9 +5,11 @@ import SkillsItem from './SkillItem';
 function Skills() {
 	return (
 		<div className="flex flex-col items-center justify-center px-5 m-10 bg-gray">
-			<h1 className="text-lg md:text-2xl font-bold p-5">Top Skills</h1>
+			<h1 className="text-lg md:text-2xl font-bold text-greenDark p-5">
+				Top Skills
+			</h1>
 			<div className="flex md:flex-row ">
-				<div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
+				<div className="grid grid-cols-2 md:grid-cols-3 gap-4">
 					{skills.map((skills) => (
 						<SkillsItem imgUrl={skills.imgUrl} />
 					))}
