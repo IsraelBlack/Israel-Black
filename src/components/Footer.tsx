5fdceecec4e0fea9b8ff3a0ba1c8b13ace7aaaca
Israel Black <israelblack98@outlook.com>
2023-11-09 11:43:04 +1300

    year updates auto

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
 	return (
 		<div className="p-0.5 text-center text-sm md:text-base b-0">
-			<p>© Copyright 2023</p>
+			<p>&copy; {new Date().getFullYear()} Israel Black</p>
 			<p>Built with React by Israel Black</p>
 		</div>
 	);
